c11e845d0289ab1a863d75ad6d700a3d58d5dd34
anayatc@gmail.com <anayatc@sky.com>
2017-06-24 15:30:39 +0100

    created template file for form

--- phantom-ghost-name-picker/main.py
+++ phantom-ghost-name-picker/main.py
@@ -15,7 +15,9 @@
 # limitations under the License.
 #
 import webapp2
+import os
 from google.appengine.ext import db
+from google.appengine.ext.webapp import template
 
 
 class Name(db.Model):
@@ -25,7 +27,8 @@ class Name(db.Model):
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello again world!')
+        path = os.path.join(os.path.dirname(__file__), 'templates/form.html')
+        self.response.out.write(template.render(path, {}))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
