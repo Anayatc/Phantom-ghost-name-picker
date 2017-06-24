bf40f429c23bf6bfe14f29385afc51f279d25ffb
anayatc@gmail.com <anayatc@sky.com>
2017-06-24 16:48:50 +0100

    created form template and linked to index page

--- phantom-ghost-name-picker/main.py
+++ phantom-ghost-name-picker/main.py
@@ -26,10 +26,22 @@ class Name(db.Model):
 
 
 class MainHandler(webapp2.RequestHandler):
+
     def get(self):
         path = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+        self.response.out.write(template.render(path, {}))
+
+    def post(self):
+        path = os.path.join(os.path.dirname(__file__), 'templates/form.html')
         self.response.out.write(template.render(path, {}))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
+
+
+def main():
+    app.run()
+
+if __name__ == '__main__':
+    main()
